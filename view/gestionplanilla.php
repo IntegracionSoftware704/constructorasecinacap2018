49dce3d1bb8bf25a0c429791e669f3a1f4a20777
44teru <layo90@hotmail.com>
2018-05-11 16:39:56 -0300

    Gestiona planillas, de acuerdo al CRUD

---
+++ view/gestionplanilla.php
@@ -0,0 +1,45 @@
+<div class="container">
+    <div class="col-sm-10 col-md-10">
+    <table class="table table-condensed">
+    <thead>
+    <a href="#" class="btn btn-success pull-right"><b>+</b>Agregar planilla</a>
+        <tr>
+            <th>Dato 1</th>
+            <th>Dato 2</th>
+            <th>Dato 3</th>
+            <th>Dato 4</th>
+            <th>Dato 5</th>
+            <th>Dato 6</th>
+            <th class="text-center">Acciones</th>
+        </tr>
+    </thead>
+            <tr>
+                <td>1</td>
+                <td>News</td>
+                <td>News Cate</td>
+                <td>1</td>
+                <td>News</td>
+                <td>News Cate</td>
+                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span>Editar</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span>Eliminar</a></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Products</td>
+                <td>Main Products</td>
+                <td>1</td>
+                <td>News</td>
+                <td>News Cate</td>
+                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span>Editar</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span>Eliminar</a></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Blogs</td>
+                <td>Parent Blogs</td>
+                <td>1</td>
+                <td>News</td>
+                <td>News Cate</td>
+                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span>Editar</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span>Eliminar</a></td>
+            </tr>
+    </table>
+    </div>
+</div>
