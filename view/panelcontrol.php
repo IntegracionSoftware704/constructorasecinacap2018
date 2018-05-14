ad82f30e7897d61cdaac57fe30c2b0ea15b32e35
44teru <layo90@hotmail.com>
2018-05-14 19:11:26 -0400

    editado por conflictos con menu-bar, añadido pagina inicio

--- view/panelcontrol.php
+++ view/panelcontrol.php
@@ -7,51 +7,82 @@
     }
 ?>
 
+<div class="menu-fila">
+    <div class="container">
+<nav class="navbar-default">
+<div class="navbar-header">
+  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+  <span class="sr-only">Toggle navigation</span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#">Panel de control</a>
+</div>
+
+<div class="collapse navbar-collapse js-navbar-collapse">
+<ul class="nav navbar-nav">
+    <li><a href="panelcontrol.php">Inicio</a></li>
+    <li><a href="gestionplanilla.php">Gestion planillas</a></li>
+    <li><a href="gestionmateriales.php">Gestion Materiales</a>
+    <li><a href="cotizaciones.php">Cotizacion</a>
+    </li>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+</ul>
+</div><!-- /.nav-collapse -->
+</nav>
+</div>
+</div>
 <div class="container">
-	<div class="col-sm-2">
-    <nav class="nav-sidebar">
-		<ul class="nav tabs">
-          <li class="active"><a href="#tab1" data-toggle="tab">Cotizaciones</a></li>
-          <li class=""><a href="#tab2" data-toggle="tab">Gestion planillas</a></li>
-          <li class=""><a href="#tab3" data-toggle="tab">Stet clita</a></li>
-		</ul>
-	</nav>
-		<div style="background-color:#ffffff;"><h2> <br><br><br><br><br><br></h2></div>
-</div>
-<!-- tab content -->
-<div class="tab-content">
-<div class="tab-pane active text-style" id="tab1">
-  <h2>Cotizaciones</h2>
-       <p>
-         <?php
-           require_once("cotizaciones.php");
-           ?>
-       </p>
-       <hr>
-</div>
-<div class="tab-pane text-style" id="tab2">
-  <h2>Gestion planillas</h2>
-    <?php
-      require_once("gestionplanilla.php");
-     ?>
-</div>
-<div class="tab-pane text-style" id="tab3">
-  <h2>Stet clita</h2>
-  <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum
-    iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla
-    facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit
-    augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,</p>
-    <hr>
-    <div class="col-xs-6 col-md-3">
-      <img src="http://placehold.it/150x150" class="img-rounded pull-right">
+  <h2>Inicio</h2>
+  <br>
+  <div class="row">
+      <div class="col-md-3" style="background:#46d140;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 1</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background: #73fe5a;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 2</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background:#46d140;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 3</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background: #73fe5a;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 4</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background:#46d140;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 5</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background: #73fe5a;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 6</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background:#46d140;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 5</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+      <div class="col-md-3" style="background: #73fe5a;">
+        <a href="#"><img src="../img/plani1.jpg" class="img-thumbnail" alt="Cinque Terre" width="300"></a>
+        <h5>Planilla 6</h5>
+        <h5><strong>Costo total: $5.000.000<strong></h5>
+      </div>
+    </div>
+    <div style="text-align: right;">
+    <a href="#">1</a> > <a href="#">2</a> > <a href="#">3</a> > ...
   </div>
 </div>
-</div>
-
-
-</div>
-
-
 <?php
     require_once("footer.php");
 ?>
