ee440cdccca37fed2f680c03ae41b474311b25cc
44teru <layo90@hotmail.com>
2018-06-04 19:41:23 -0400

    defecto de sesion corregido

--- view/laempresa.php
+++ view/laempresa.php
@@ -1,10 +1,5 @@
 <?php
-    require_once("header.php");
-    require_once("../modulos/isLogin.php");
-    if(!($estado))
-    {
-        header("Location: ../index.php");
-    }
+require_once("header.php");
 ?>
 <div class="container">
 <br>
@@ -13,92 +8,92 @@
 <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 hidden-xs">
                 <ul>
-                    
+
                     <li>
                         <a href="/nuestros-proyectos/">
                             nuestros proyectos
                         </a>
                     </li>
-                   
+
                     <li>
                         <a href="/como-comprar/">
                             cómo comprar
                         </a>
                     </li>
-                   
+
                     <li>
                         <a href="/todoesdiseno/">
                             Todo es Diseño
                         </a>
                     </li>
-                   
+
                 </ul>
                 <ul class="lite">
-                    
+
                     <li>
                         <a href="/">
                             inicio
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/oportunidades2017">
                             Oportunidades 2018
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/preguntas-frecuentes">
                             preguntas frecuentes
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/noticias">
                             noticias
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/posventa">
                             Posventa
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/trabaja-con-nosotros">
                             Trabaja con Nosotros
                         </a>
                     </li>
-                    
+
                     <li>
                         <a href="/contacto">
                             contacto
                         </a>
                     </li>
-                    
+
                 </ul>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 hidden-xs">
-                
-                    
+
+
                     <h6>
                         CONTACTO POSVENTA SANTIAGO
                     </h6>
                     <span>
                         servicioalcliente@constructorasec.cl
                     </span>
-                
-                    
+
+
                         <hr>
-                    
+
                     <h6>
                         CONTACTO POSVENTA SUR
                     </h6>
                     <span>
                         posventasur@constructorasec.cl
                     </span>
-                
+
 
                 <h6 class="mt40">
                     Siguenos en nuestras redes sociales
@@ -130,35 +125,35 @@
                     <div class="rich-text">© 2018 Constructora SEC - La empresa se reserva la facultad de efectuar cambios en los proyectos. Al efectuar su compra verifique las características de los mismos. Las imágenes y textos contenidos en este sitio web son meramente ilustrativos y referenciales, por lo que podrían no representar exactamente la realidad. Los precios están sujetos a disponibilidad de los productos. Exija su cotización en las salas y puntos de venta. La aprobación del crédito hipotecario y obtención de los respectivos subsidios o beneficios sociales son de exclusiva responsabilidad del cliente. Todos los derechos reservados. Es obligación ver nota legal.&nbsp;<p><br></p><p></p><p>*Cálculo de dividendo referencial correspondiente al precio desde, calculado a 25 años plazo, con tasa fija 3,5%, 20% de pie y UF al día 18 de junio del 2017. La tasa de interés es referencial, consulte con su banco. Cálculo incluye seguros de desgravamen e incendio basado en simulación de Banco Santander.<br></p><p></p><p></p></div>
                 </div>
                 <ul class="politicas centrado-mobile">
-                    
+
                     <li>
                         <a data-toggle="modal" data-target="#aviso-legal" data-action-title="Aviso Legal" data-action-description="<p>Este aviso legal regula el uso del sitio web que Socovesa S.A. pone a disposición de los usuarios de Internet a través de la dirección&nbsp;<a href=&quot;http://www.socovesa.cl./&quot;><strong>http://www.socovesa.cl</strong></a><strong><a href=&quot;http://www.socovesa.cl./&quot;>.</a></strong></p><p><strong>Todas las páginas del sitio son meramente informativas.</strong></p><ul><li>Las ambientaciones de las imágenes dentro del sitio son solo ilustrativas y no constituyen necesariamente una representación de la realidad.</li><li>Los precios publicados están sujetos a disponibilidad. Estos pueden variar en cualquier momento.</li><li>La fecha de entrega es de carácter referencial y está sujeta a la recepción municipal del proyecto.</li><li>Las superficies, planos e información entregada de los proyectos son sólo referenciales.</li><li>Precios, tasas de interés y resultados de los servicios de simulación de crédito hipotecario son referenciales.</li><li>Al momento de efectuar tu compra verifica las características y especificaciones de cada proyecto. Estas podrían experimentar algunas modificaciones. Realiza tu cotización en sala de ventas.</li></ul><p><strong>Del uso de la página web:&nbsp;</strong></p><ul><li>El usuario podrá visualizar los contenidos publicados e incluso imprimirlos, copiarlos y almacenarlos en el disco duro de su ordenador o en cualquier otro soporte físico siempre y cuando sea, única y exclusivamente, para su uso personal y privado, quedando, por tanto, terminantemente prohibida su utilización con fines comerciales, su distribución, así como su modificación, alteración o descompilación.</li><li>Socovesa S.A. se reserva el derecho de efectuar sin previo aviso las modificaciones que considere oportunas en su website, pudiendo cambiar, suprimir o anadir tanto los contenidos y servicios que se presten a través de la misma como la forma en la que éstos aparezcan presentados o localizados en sus websites.</li><li>Para utilizar algunos de los Servicios, los usuarios deben proporcionar previamente a Socovesa S.A. ciertos datos de carácter personal a través de un formulario de registro. Mediante el envío del correspondiente formulario de registro, el usuario acepta y autoriza que sus Datos Personales sean objeto de tratamiento automatizado por parte de Socovesa S.A. La empresa podrá utilizar la información de contacto de los usuarios para enviar información sobre la empresa. También puede usar la información personal para determinar los datos demográficos de nuestros usuarios y distribuir información estadística y general del mercado. Estos datos demográficos siempre representan información agregada y nunca identifican a un usuario específico.</li><li>El Usuario es consciente de y acepta voluntariamente que el uso del Servicio y de los Contenidos tiene lugar, en todo caso, bajo su única y exclusiva responsabilidad.</li><li>Cálculo de dividendo referencial correspondiente al precio desde, calculado a 25 anos plazo con tasa fija 5% y UF al día. La tasa de interés es referencial, consulte con su banco. Cálculo no incluye seguros.</li></ul><p>Si tienes alguna consulta acerca de nuestra declaración de seguridad y privacidad, puedes ponerte en contacto con nuestra empresa mediante un correo electrónico a socovesa@socovesa.cl</p><br/>">
                             Aviso Legal
                         </a>
                     </li>
 
-                    
+
                     <li>
                         <a data-toggle="modal" data-target="#politica-de-privacidad" data-action-title="Política de Privacidad" data-action-description="<p>Este sitio web utiliza Google Analytics, un servicio analítico de web prestado por Google, Inc.</p><p>Google Analytics utiliza cookies, archivos de texto que se descargan a su computador o dispositivo, con el fin de entregar información a analizar sobre el uso que hacen los usuarios de nuestro sitio web.</p><p>La información generada acerca del website (incluyendo su dirección IP) será directamente transmitida y archivada por Google en los servidores de Estados Unidos.</p><p>Inmobiliaria Socovesa usará esta información para mejorar el conocimiento y rendimiento sobre nuestro sitio web y su efectividad respecto a la información que entrega.</p><p>Google no asociará su dirección IP con ningún otro dato del que disponga.</p><p>Todo usuario puede deshabilitar el uso de cookies mediante la configuración apropiada, y según lo estipula cada navegador.</p><p>Al utilizar este sitio web Ud. autoriza el tratamiento de información acerca de Ud. por Google en la forma y fines arriba indicados.</p><p>Este sitio web utiliza además la función de remarketing con Google Analytics la cual permite publicar anuncios online, por lo que otros proveedores, incluido Google, podrán mostrar publicidad de nuestra empresa en sitios de Internet, basándose en la información adquirida a través de las cookies almacenadas en sus visitas a nuestro sitio web.</p><p>Usted tiene la potestad de inhabilitar Google Analytics para publicidad de display y personalizar los anuncios de la Red de Display de Google mediante el Administrador de preferencias de anuncios:&nbsp;<a href=&quot;https://www.google.com/settings/ads&quot;>https://www.google.com/settings/ads</a>&nbsp;o utilizar el complemento de inhabilitación para navegadores de Google Analytics:<a href=&quot;https://tools.google.com/dlpage/gaoptout/&quot;>https://tools.google.com/dlpage/gaoptout/</a></p>">
                             Política de Privacidad
                         </a>
                     </li>
 
-                    
+
                     <li>
                         <a data-toggle="modal" data-target="#campana-90-financiamiento" data-action-title="Campaña 90% financiamiento" data-action-description="Hasta en 30 años plazo<ul><li>Para clientes cuya renta mensual califique para Banca Preferente. Banca Premium o Premium Plus (1)</li><li>Con
- tu Tarjeta de Crédito Scotiabank puedes pagar el pie de tu vivienda 
-hasta en 18 cuotas sin interés (para montos promedio de $7.000.000 CAE 
-8,76%)(2)</li></ul>Infórmese sobre la garantía estatal a los depósitos 
-en su banco o en www.sbif.cl.® Marca registrada de The Bank of Nova 
-Scotia, utilizada bajo licencia.<br/>(1) Para clientes Banca Preferente 
-con renta mensual desde $1.000.000 a $2.999.000 y Premium/Premium Plus 
+ tu Tarjeta de Crédito Scotiabank puedes pagar el pie de tu vivienda
+hasta en 18 cuotas sin interés (para montos promedio de $7.000.000 CAE
+8,76%)(2)</li></ul>Infórmese sobre la garantía estatal a los depósitos
+en su banco o en www.sbif.cl.® Marca registrada de The Bank of Nova
+Scotia, utilizada bajo licencia.<br/>(1) Para clientes Banca Preferente
+con renta mensual desde $1.000.000 a $2.999.000 y Premium/Premium Plus
 con renta mensual desde $3.000.000 en adelante.<br/>(2) Tasa de interés para clientes Preferentes 0,89% y para clientes Premium/Premium Plus 0,69%">
                             Campaña 90% financiamiento
                         </a>
                     </li>
 
-                    
+
                 </ul>
             </div>
         </div>
@@ -168,25 +163,25 @@ con renta mensual desde $3.000.000 en adelante.<br/>(2) Tasa de interés para cl
             BANCOS QUE FORMAN PARTE DEL CONVENIO
         </h3>
         <div class="slide-bancos owl-carousel owl-theme" style="opacity: 1; display: block;">
-            
+
             <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 4560px; left: 0px; display: block; transform: translate3d(0px, 0px, 0px);"><div class="owl-item" style="width: 228px;"><div class="contenedor-banco">
                 <img src="../img/1.width-140.jpg" width="96" height="48" alt="Bci">
             </div></div><div class="owl-item" style="width: 228px;"><div class="contenedor-banco">
                 <img src="../img/3.width-140.jpg" width="136" height="48" alt="Banco Falabella">
             </div></div><div class="owl-item" style="width: 228px;"><div class="contenedor-banco">
                 <img src="../img/4.width-140.jpg" width="140" height="26" alt="Banco Estado">
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
+
+
         <div class="owl-controls"><div class="owl-buttons"><div class="owl-prev fa fa-angle-left fa-5"></div><div class="owl-next fa fa-angle-right fa-5"></div></div></div></div>
     </div>
 </div>
