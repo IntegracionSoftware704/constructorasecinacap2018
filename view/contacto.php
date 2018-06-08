4a5cd73285a26b73a1edc6b625f25ff35d45ccff
44teru <layo90@hotmail.com>
2018-06-08 01:46:04 -0400

    Edicion de pagina contacto.php

--- view/contacto.php
+++ view/contacto.php
@@ -1,130 +1,47 @@
 <?php
-    require_once("header.php");
-    require_once("../modulos/isLogin.php");
-    if(!($estado))
-    {
-        header("Location: ../index.php");
-    }
+require_once("header.php");
 ?>
-
 <div class="container">
+  <div class="row">
+    <h4>CONTACTO</h4>
+    <div>Si desea enviarnos una consulta, porfavor complete el siguiente formulario:</div>
+    <div class="col-md-6">
+      <div class="form-area">
+        <form method="post" action="">
+        <br style="clear:both">
+                    <h3 style="margin-bottom: 25px; text-align: center;">Formulario de contacto</h3>
+    				<div class="form-group">
+						<input type="text" class="form-control" id="name" name="name" placeholder="Nombre Apellido">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" onkeypress="return controltag(event)" id="mobile" name="mobile" placeholder="Telefono - Celular">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Asunto">
+					</div>
+                    <div class="form-group">
+                    <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Mensaje" maxlength="300" rows="7"></textarea>
+                        <span class="help-block"><p id="characterLeft" class="help-block ">Has llegado al limite de caracteres</p></span>
+                    </div>
 
-
-
-<div class="row">
-<br>
-<div class="Titulo">
-<h4>
-Contacto
-</h4>
-<br>
-<div class="enunciado">
- <p>Si desea enviarnos una consulta, porvafor complete el siguiente formulario </p>
+        <button id="submit" name="submit" class="btn btn-primary pull-right" onclick="javascript:alert('Se necesita de un servidor SMTP para poder enviar el formulario de contacto, sorry :( \n\nDanilo');">Enviar</button>
+        </form>
+    </div>
+    </div>
+    <div class="col-md-6">
+        <h4>Informaciones</h4>
+        <div><a href="mailto:contacto@constructorasec.com">contacto@constructorasec.com</a></div>
+        <div>Telefono: +63 2232323 / +56 2232323</div>
+        <div>Celular: +569 85698745</div>
+        <div>Direccion: Av. Vitacura 10151, Vitacura, Santiago</div>
+        <div><iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d6960.804571331352!2d-73.2102507822143!3d-39.80538485966414!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1528431854334" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+    </div>
+  </div>
 </div>
-</div>
-</div>
-
-<section class="main row">
-<article class="col-md-6">
-<div class="scfSectionContent">
-				<div id="" class="">
-					<label for="" id="" class="">Nombre<span class="scfRequired">*</span></label><div class="">
-						<input name="" type="text" maxlength="256" id="" class="scfSingleLineTextBox"><span id="" class="" style="display:none;">Nombre must have at least 0 and no more than 256 characters.</span><span id="" class="" style="display:none;">The value of the Nombre field is not valid.</span>
-					</div>
-				</div><div id="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B_scope" class="scfSingleLineTextBorder fieldid.%7b04802AA8-DFCF-48F7-A08F-513E6FA0B43B%7d name.Last+name">
-					<label for="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B_text" class="scfSingleLineTextLabel">Apellidos<span class="scfRequired">*</span></label><div class="scfSingleLineGeneralPanel">
-						<input name="form_E76EDAD43B90478C9F8A328ABA94380B$field_04802AA8DFCF48F7A08F513E6FA0B43B" type="text" maxlength="256" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B" class="scfSingleLineTextBox"><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B6ADFFAE3DADB451AB530D89A2FD0307B_validator" class="scfValidator trackevent.%7bF3D7B20C-675C-4707-84CC-5E5B4481B0EE%7d fieldid.%7b04802AA8-DFCF-48F7-A08F-513E6FA0B43B%7d inner.1" style="display:none;">Apellidos must have at least 0 and no more than 256 characters.</span><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_04802AA8DFCF48F7A08F513E6FA0B43B070FCA141E9A45D78611EA650F20FE77_validator" class="" style="display:none;">The value of the Apellidos field is not valid.</span>
-					</div>
-				</div><div id="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A2197_scope" class="scfSingleLineTextBorder fieldid.%7bDBBC2002-A0BA-4D10-B4B9-8142191A2197%7d name.Company+name">
-					<label for="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A2197" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A2197_text" class="scfSingleLineTextLabel">Empresa</label><div class="scfSingleLineGeneralPanel">
-						<input name="form_E76EDAD43B90478C9F8A328ABA94380B$field_DBBC2002A0BA4D10B4B98142191A2197" type="text" maxlength="256" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A2197" class="scfSingleLineTextBox"><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A21976ADFFAE3DADB451AB530D89A2FD0307B_validator" class="scfValidator trackevent.%7bF3D7B20C-675C-4707-84CC-5E5B4481B0EE%7d fieldid.%7bDBBC2002-A0BA-4D10-B4B9-8142191A2197%7d inner.1" style="display:none;">Empresa must have at least 0 and no more than 256 characters.</span><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_DBBC2002A0BA4D10B4B98142191A2197070FCA141E9A45D78611EA650F20FE77_validator" class="scfValidator trackevent.%7b844BBD40-91F6-42CE-8823-5EA4D089ECA2%7d fieldid.%7bDBBC2002-A0BA-4D10-B4B9-8142191A2197%7d inner.1" style="display:none;">The value of the Empresa field is not valid.</span>
-					</div>
-				</div><div id="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC72_scope" class="scfSingleLineTextBorder fieldid.%7bC69446E7-9667-4DC0-95FA-0E288B69BC72%7d name.Job+title">
-					<label for="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC72" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC72_text" class="scfSingleLineTextLabel">Cargo</label><div class="scfSingleLineGeneralPanel">
-						<input name="form_E76EDAD43B90478C9F8A328ABA94380B$field_C69446E796674DC095FA0E288B69BC72" type="text" maxlength="256" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC72" class="scfSingleLineTextBox"><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC726ADFFAE3DADB451AB530D89A2FD0307B_validator" class="scfValidator trackevent.%7bF3D7B20C-675C-4707-84CC-5E5B4481B0EE%7d fieldid.%7bC69446E7-9667-4DC0-95FA-0E288B69BC72%7d inner.1" style="display:none;">Cargo must have at least 0 and no more than 256 characters.</span><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_C69446E796674DC095FA0E288B69BC72070FCA141E9A45D78611EA650F20FE77_validator" class="scfValidator trackevent.%7b844BBD40-91F6-42CE-8823-5EA4D089ECA2%7d fieldid.%7bC69446E7-9667-4DC0-95FA-0E288B69BC72%7d inner.1" style="display:none;">The value of the Cargo field is not valid.</span>
-					</div>
-				</div><div id="form_E76EDAD43B90478C9F8A328ABA94380B_field_5EC32AC29B8D44DAA4DAEDF96B76C8D9_scope" class="scfSingleLineTextBorder fieldid.%7b5EC32AC2-9B8D-44DA-A4DA-EDF96B76C8D9%7d name.Phone+number">
-					<label for="form_E76EDAD43B90478C9F8A328ABA94380B_field_5EC32AC29B8D44DAA4DAEDF96B76C8D9" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_5EC32AC29B8D44DAA4DAEDF96B76C8D9_text" class="scfSingleLineTextLabel">Numero de teléfono</label><div class="scfSingleLineGeneralPanel">
-						<input name="form_E76EDAD43B90478C9F8A328ABA94380B$field_5EC32AC29B8D44DAA4DAEDF96B76C8D9" type="text" maxlength="256" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_5EC32AC29B8D44DAA4DAEDF96B76C8D9" class="scfSingleLineTextBox"><span id="" class="" style="display:none;">Numero de teléfono must have at least 0 and no more than 256 characters.</span><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_5EC32AC29B8D44DAA4DAEDF96B76C8D9070FCA141E9A45D78611EA650F20FE77_validator" class="scfValidator trackevent.%7b844BBD40-91F6-42CE-8823-5EA4D089ECA2%7d fieldid.%7b5EC32AC2-9B8D-44DA-A4DA-EDF96B76C8D9%7d inner.1" style="display:none;">The value of the Numero de teléfono field is not valid.</span>
-					</div>
-				</div><div id="form_E76EDAD43B90478C9F8A328ABA94380B_field_4B1EEDECA70C46719F46E010ABCC5936_scope" class="scfSingleLineTextBorder fieldid.%7b4B1EEDEC-A70C-4671-9F46-E010ABCC5936%7d name.Email+Address+at+Work">
-					<label for="form_E76EDAD43B90478C9F8A328ABA94380B_field_4B1EEDECA70C46719F46E010ABCC5936" id="form_E76EDAD43B90478C9F8A328ABA94380B_field_4B1EEDECA70C46719F46E010ABCC5936_text" class="scfSingleLineTextLabel">Correo electrónico<span class="scfRequired">*</span></label><div class="scfSingleLineGeneralPanel">
-						<input name="" type="text" maxlength="256" id="" class="scfSingleLineTextBox"><span id="" class="" style="display:none;">Correo electrónico must have at least 0 and no more than 256 characters.</span><span id="form_E76EDAD43B90478C9F8A328ABA94380B_field_4B1EEDECA70C46719F46E010ABCC5936070FCA141E9A45D78611EA650F20FE77_validator" class="" style="display:none;">The value of the Correo electrónico field is not valid.</span>
-					</div>
-				</div><div id="" class="">
-					<label for="" id="" class="scfDropListLabel">Motivo:<span class="scfRequired">*</span></label><div class="scfDropListGeneralPanel">
-						
-					</div>
-				</div><div id="" class="">
-					<label for="formtextArea" id="textArea" class="scfMultipleLineTextLabel">Mensaje:<span class="scfRequired">*</span></label><div class="scfMultipleLineGeneralPanel">
-						<textarea name="form" rows="4" cols="20" id="form" class="scfMultipleLineTextBox"></textarea><span class="scfMultipleLineTextUsefulInfo" style="display:none;"></span><span id="form" class="" style="display:none;">Mensaje: must have at least 0 and no more than 512 characters.</span><span id="" class="" style="display:none;">The value of the Mensaje: field is not valid.</span>
-					</div>
-                    <input type="submit" value="Enviar" class="btn btn-success">
-				</div>
-			</div>
-
-
-</article>
-<aside class="col-md-6">
-<h4>Informaciones</h4>
-<p>Contacto@constructorasec.cl
-</p>
-<br>
-<br>
-<p>
-63 2232323 / 63 2232323
-</p>
-<p>
-+569 98765432
-</p>
-<br>
-<br>
-<p>
-Av. Vitacura 10.151, Vitacura, Santiago
-</p>
-
-</aside>
-
-</section>
-
-
-
-
-
-</div>
-
-</div>
-
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<br><br>
 <?php
     require_once("footer.php");
 ?>
