d27685bd208ebebb76d39b0bc483fdbb3e2731bd
AldoVera <aestebanveraz@gmail.com>
2018-06-25 14:43:20 -0400

    mysql nombre base de datos

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 class Conectar{
     public static function conexion()
     {
-        $conexion=new mysqli("localhost", "root", "","mysql-contructora");
+        $conexion=new mysqli("localhost", "root", "","mysql");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
